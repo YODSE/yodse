4a2755bf249115a17fe6dc90324d951cd9a71524
PillarDevelopment <2622610@gmail.com>
2018-04-10 15:20:57 +0300

    форматировал исправил burn заменил софткапы и хардкапы добавил рефанд

--- Ver_2.0._YODSE.sol
+++ Ver_2.0._YODSE.sol
@@ -1,28 +1,19 @@
 /*
 PreICO
+Срок проведения с 15.04.2018г. по 30.04.2018г.
 
 Покупатели токенов на pre-ICO и ICO на любую сумму становятся участниками реферальной программы, и им выплачивается вознаграждение в размере 5% от суммы покупок привлеченных рефералов.
 Первым покупателям токенов на pre-ICO и ICO на сумму не менее 1000 USD будет предусмотрен повышенный реферальный процент по сравнению с обычными держателями токенов и составит 7%.
 Реферальный фонд токенов в размере 10% от общего выпущенного количества блокируется до 01.01.2019г. (за исключением реферальных выплат в период pre-ICO и ICO),и дальнейшие выплаты рефералам будут доступны с 01.01.2019г. в соответствии с реферальной программой.
-
-+ Оракул, меняющий цену 2 раза в сутки(сделано)
-
-+ блокмрованме средств реферальых
 */
 
 pragma solidity ^0.4.21;
-
-import "https://github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
 /*
 * @author Ivan Borisov (2622610@gmail.com) (Github.com/pillardevelopment)
 * @dev Source code hence -
 * https://github.com/PillarDevelopment/Barbarossa-Git/blob/master/contracts/BarbarossaInvestToken.sol
-*
 */
-/*********************************************************************************************************************
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
+
 import "https://github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
 
 library SafeMath {
@@ -37,9 +28,9 @@ library SafeMath {
     }
 
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b > 0); // Solidity automatically throws when dividing by 0
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
         uint256 c = a / b;
-        assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return c;
     }
 
@@ -47,6 +38,7 @@ library SafeMath {
         assert(b <= a);
         return a - b;
     }
+
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
@@ -73,12 +65,13 @@ contract TokenERC20 is Ownable {
     string public name;
     string public symbol;
     uint256 public decimals = 18;
-    uint256 DEC = 10 ** decimals;
+    uint256 DEC = 10 ** uint256(decimals);
     address public owner;  //0x6a59CB8b2dfa32522902bbecf75659D54dD63F95
     // all tokens
     uint256 public totalSupply;
     // tokens for sale
     uint256 public avaliableSupply;  // totalSupply - all reserve
+
 
     mapping (address => uint256) public balanceOf;
 
@@ -114,9 +107,9 @@ contract TokenERC20 is Ownable {
         _transfer(msg.sender, _to, _value);
     }
 
-    function burn(uint256 _value) public onlyOwner returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+    function burn(uint256 _value) internal onlyOwner returns (bool success) {
+        require(balanceOf[this] >= _value);   // Check if the sender has enough
+        balanceOf[this] -= _value;            // Subtract from the sender
         totalSupply -= _value;                      // Updates totalSupply
         avaliableSupply -= _value;
         emit Burn(msg.sender, _value);
@@ -130,21 +123,16 @@ contract TokenERC20 is Ownable {
 contract YodseCrowdsale is TokenERC20, usingOraclize {
     using SafeMath for uint;
 
-    uint public constant softcapPreSale = 1000000*1e18; // 1,000,000 USD
-
-    uint public constant hardCapPreIco = 3000000*1e18; // 3,000,000 USD
-
-    uint public constant softCapMainSale = 7000000*1e18; // 7,000,000 USD
-
-    uint public constant hardCapMainISale = 40000000*1e18; // 40,000,000 USD
-
+    uint256 public softcapPreSale = 1000000000000000000000000/usdToEther;
+    uint256 public  hardCapPreIco = 3000000000000000000000000/usdToEther;
+    uint256 public softCapMainSale = 7000000000000000000000000/usdToEther;
+    uint256 public  hardCapMainISale = 40000000000000000000000000/usdToEther;
 
     // address beneficiary 0x6a59CB8b2dfa32522902bbecf75659D54dD63F95
     address beneficiary = 0x6a59cb8b2dfa32522902bbecf75659d54dd63f95;
 
-    uint public startPreIcoDate = 1523754000; // 04/15/2018 @ 1:00am (UTC)
+    uint public startPreIcoDate = now; // 04/15/2018 @ 1:00am (UTC)
     uint public endPreIcoDate = 1525050000; // 04/30/2018 @ 1:00am (UTC)
-
     uint public startIcoDate = 1526346000; // 05/15/2018 @ 1:00am (UTC)
     uint public endIcoDate = 1529888400; // 06/25/2018 @ 1:00am (UTC)
 
@@ -163,39 +151,35 @@ contract YodseCrowdsale is TokenERC20, usingOraclize {
     // variable counts the number of investors after call sell function.
     uint256 public investors;
 
-    uint256 public etherBuyPrice; //0,001 ether
-    uint256 public tokenNominal = 1000000000000000000; // 1 token = 1 USD
-    uint public usdToEther;
-
     address team = 0xcc2fb3e7f4bc1b8948fa5163319cfe728dd1a471;
     address referal = 0x7c2c0ae6bac57e7d01198e6024b181f04e109faf;
     address consult = 0x135a7a2986ae47c8b054f1e0e1f1e0be0f58435b;
     address marketing = 0xb76329f2531675e8b3b2873d68c452d83aae072c;
     address bounty = 0xd340b5fcabf905f82ab5bb199925adcba11adcdb;
-    address[] public refererArray;
 
     bool distribute = false;
     uint public weisRaised;
     bool public isFinalized = false;
 
-    event Finalized();
+    event Finalized(address _who, uint256 _data);
     event newOraclizeQuery(string description);
     event newPriceTicker(uint price);
     event AuthorizedReferer(address _referer, uint256 _time);
 
     mapping (address => bool) public onChain;
-    address[] public tokenHolders;  // tokenHolders.length
-    mapping(address => uint) public balances; // храним адрес инвестора и исколь он инвестировал
-    mapping(address => uint) public tokenFrozenTeam; // safe address developers
-    mapping(address => uint) public tokenFrozenReserve; // safe address ReserveFond
-    mapping(address => uint) public tokenFrozenConsult; // safe address advisers
+    address[] public tokenHolders;
+    mapping(address => uint) public balances;
+    mapping(address => uint) public tokenFrozenTeam;
+    mapping(address => uint) public tokenFrozenReferal;
+    mapping(address => uint) public tokenFrozenConsult;
     mapping(address => uint256) public investedEther;
-    mapping(address=>bool) public refererlist;
+
+    uint256 public tokenNominal = 1000000000000000000; // 1 token = 1 USD
+    uint public usdToEther = 400;
+    uint256 public etherBuyPrice = tokenNominal.div(usdToEther); //0,001 ether
 
 
-    function YodseCrowdsale() public TokenERC20(100000000, "Your Open Direct Sales Ecosystem", "YODSE") {
-
-    }
+    function YodseCrowdsale() public TokenERC20(100000000, "Your Open Direct Sales Ecosystem", "YODSE") {}
 
     modifier isUnderHardCap() {
         require( weisRaised <= hardCapMainISale);
@@ -237,7 +221,7 @@ contract YodseCrowdsale is TokenERC20, usingOraclize {
         } else {
             _amount = _amount.add(withDiscount(_amount, 0));
         }
-        require(amount > avaliableSupply); // проверка что запрашиваемое количество токенов меньше чем есть на балансе
+
         avaliableSupply -= _amount;
         _transfer(this, _investor, _amount);
         if (!onChain[msg.sender]) {
@@ -250,21 +234,8 @@ contract YodseCrowdsale is TokenERC20, usingOraclize {
     function withDiscount(uint256 _amount, uint _percent) internal pure returns (uint256) {
         return ((_amount * _percent) / 100);
     }
-    //
-    function setStartPreICODate(uint256 newStartPreIcoDate) public onlyOwner{
-        startPreIcoDate = newStartPreIcoDate;
-    }
-
-    function setendPreIcoDate(uint256 newendPreIcoDate) public onlyOwner{
-        endPreIcoDate = newendPreIcoDate;
-    }
-
-    function setStartICODate(uint256 newstartIcoDate) public onlyOwner{
-        startIcoDate = newstartIcoDate;
-    }
-
     // функция изменения даты окончания ICO собственником контракта
-    function setEndICODate(uint newEndIcoDate) public onlyOwner {
+    function setEndData(uint newEndIcoDate) public onlyOwner {
         endIcoDate  = newEndIcoDate;
     }
     // функция для отправки эфира с контракта
@@ -282,56 +253,21 @@ contract YodseCrowdsale is TokenERC20, usingOraclize {
         //beneficiary.transfer(msg.value); // средства отправляюся на адрес бенефециара
         // добавляем получаные средства в собранное
         weisRaised = weisRaised.add(msg.value);
-
         // добавляем в адрес инвестора количество инвестированных эфиров
+        //balances[msg.sender] = balances[msg.sender].add(msg.value);
         investors  += 1;
         investedEther[msg.sender] = investedEther[msg.sender].add(msg.value);
-
-        //balances[msg.sender] = balances[msg.sender].add(msg.value);
-
-        // Авторизация в реферальной программе
-        inRefererList(msg.sender);
-
-        /*
-        */
-
-        //if(msg.data.length == 20) { // проверяем что в msd.data лежит адрес
-        //  address localref = bytesToAddress(bytes(msg.data)); // преобразовываем msg.data в адрес
-        //require(localref != msg.sender); // проверяем что там не адрес отправителя
-        //require(isRefererlisted(localref)); // проверяем что адрес реферера участвует в програме - чтобы не мог запилить аффилированный адрес
-        //uint refererTokens = msg.value.div(etherBuyPrice); // посчитали количество бонусных токенов
-        //refererTokens = withDiscount(refererTokens, 5);
-        //_transfer(referal, localref, refererTokens);
-        //}
     }
-
-    // функция преобразования msg.data в address
-    function bytesToAddress(bytes source) internal pure returns(address) {
-        uint result;
-        uint mul = 1;
-        for(uint i = 20; i > 0; i--) {
-            result += uint8(source[i-1])*mul;
-            mul = mul*256;
-        }
-        return address(result);
-    }
-
-
-
 
     function finalize() onlyOwner public {
-        require(!isFinalized);
-        require(now > endIcoDate);
-
-        finalization();
-        emit Finalized();
-
+        //require(!isFinalized);
+        //require(now > endIcoDate || weisRaised >= hardCapMainISale);
+        emit Finalized(msg.sender, now);
+        burn(avaliableSupply);
         isFinalized = true;
         emit Burn(msg.sender, avaliableSupply);
     }
 
-    function finalization() internal pure {
-    }
 
     function distributionTokens() public onlyOwner {
         require(!distribute);
@@ -343,20 +279,19 @@ contract YodseCrowdsale is TokenERC20, usingOraclize {
 
         _transfer(this, marketing, marketingReserve*DEC); // immediately marketing all
         _transfer(this, bounty, bountyReserve*DEC);
-        tokenFrozenReserve[referal] = tokenFrozenReserve[referal].add(10000000*DEC);  // immediately reserve all
+        tokenFrozenReferal[referal] = tokenFrozenReferal[referal].add(10000000*DEC);  // immediately reserve all
         avaliableSupply -= 40000000*DEC;
         distribute = true;
     }
-    // функция выдачи замороженных токенов членам команды
     function tokenTransferFromHolding(address) public  holdersSupport {
         require(now > endIcoDate);
         // !!! reserve - 10 000 000 после 1.1.2019
         if (msg.sender == referal && now > 1546300801) { // 1546300801 -  01/01/2019 @ 12:00am (UTC)
-            require(tokenFrozenReserve[referal] == 10000000*DEC);  // не может быть меньше так как даже если они выведут токены - на меппинг это не отразится
+            require(tokenFrozenReferal[referal] == 10000000*DEC);  // не может быть меньше так как даже если они выведут токены - на меппинг это не отразится
             //require(tokenFrozenReserve[reserve] == 7500000*DEC;);
             _transfer(beneficiary, referal, 10000000*DEC);
             balanceOf[beneficiary] = balanceOf[beneficiary].sub(10000000*DEC); // списали с бенефициара
-            tokenFrozenReserve[referal] = 0; // списали с мепинга и сделали его == 0 чтобы второй раз не вывели
+            tokenFrozenReferal[referal] = 0; // списали с мепинга и сделали его == 0 чтобы второй раз не вывели
         }
         // !!! team - 7 500 000 после 1.1.2020
         else if (msg.sender == team /* */ && now > 1577836801) { // 1577836801 - 01/01/2020 @ 12:00am (UTC)
@@ -382,6 +317,15 @@ contract YodseCrowdsale is TokenERC20, usingOraclize {
         }
     }
 
+    function refundPreICO() public {
+        require(weisRaised*usdToEther < softcapPreSale && now > endPreIcoDate);
+        require(investedEther[msg.sender] > 0);
+        uint rate = investedEther[msg.sender];
+        investedEther[msg.sender] = 0;
+        msg.sender.transfer(rate);
+        weisRaised = weisRaised.sub(rate);
+    }
+
     function __callback(bytes32 myid, string result) public {
         if (msg.sender != oraclize_cbAddress()) throw;
         usdToEther = parseInt(result, 0);
@@ -396,37 +340,12 @@ contract YodseCrowdsale is TokenERC20, usingOraclize {
             return;
         } else {
             emit newOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
-            oraclize_query(200, "URL", "json(https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD ).ETH.USD");
+            oraclize_query(43200, "URL", "json(https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD ).ETH.USD");
         }
     }
     // в случае ошибки при вызове оракула
     function manualPriceUpdate(uint _usdPrice) public onlyOwner {
         usdToEther = _usdPrice;
         etherBuyPrice = tokenNominal.div(usdToEther);
-    }
-
-    function refundPreICO() public {
-        require(weisRaised*usdToEther < softcapPreSale && now > endPreIcoDate);
-        require(investedEther[msg.sender] > 0);
-        uint rate = investedEther[msg.sender];
-        investedEther[msg.sender] = 0;
-        msg.sender.transfer(rate);
-        weisRaised = weisRaised.sub(rate);
-    }
-    // авторизовали в массиве участников программы
-    function inRefererList(address referer) internal{
-        require(!isRefererlisted(referer));
-        refererlist[referer] = true;
-        refererArray.push(referer);
-        emit AuthorizedReferer(referer, now);
-    }
-
-    function transferRefererList() public onlyOwner{
-        require(now > endIcoDate);
-        // _transfer(referal, refererArray, );
-    }
-
-    function isRefererlisted(address referer) public view returns(bool) {
-        return refererlist[referer];
     }
 }
