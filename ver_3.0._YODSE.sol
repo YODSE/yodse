719951ff4ddf768808026ea12f0a3c27d4a7d244
PillarDevelopment <2622610@gmail.com>
2018-04-19 23:43:19 +0300

    Смарт контракт токена для ИСО

--- ver_3.0._YODSE.sol
+++ ver_3.0._YODSE.sol
@@ -54,7 +54,7 @@ contract TokenERC20 is Ownable {
     uint256 public decimals = 18;
     uint256 DEC = 10 ** uint256(decimals);
     address public owner;
-    uint256 public totalSupply = 100000000;
+    uint256 public totalSupply = 100000000*DEC;
     uint256 public avaliableSupply;
 
     mapping (address => uint256) public balanceOf;
@@ -62,13 +62,11 @@ contract TokenERC20 is Ownable {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Burn(address indexed from, uint256 value);
 
+
     function TokenERC20() public
     {
-        totalSupply = initialSupply * DEC;
-        balanceOf[this] = totalSupply;
-        avaliableSupply = balanceOf[this];
-        name = tokenName;
-        symbol = tokenSymbol;
+        balanceOf[msg.sender] = totalSupply;
+        avaliableSupply = balanceOf[msg.sender];
         owner = msg.sender;
     }
 
@@ -87,12 +85,15 @@ contract TokenERC20 is Ownable {
         _transfer(msg.sender, _to, _value);
     }
 
-    function burn(uint256 _value) internal onlyOwner returns (bool success) {
-        require(balanceOf[this] >= _value);
-        balanceOf[this] -= _value;
-        totalSupply -= _value;
-        avaliableSupply -= _value;
-        emit Burn(msg.sender, _value);
-        return true;
+    function burn(uint256 _value) public {
+        _burn(msg.sender, _value);
+    }
+
+    function _burn(address _who, uint256 _value) internal {
+        require(_value <= balanceOf[_who]);
+        balanceOf[_who] = balanceOf[_who].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        emit Burn(_who, _value);
+        emit Transfer(_who, address(0), _value);
     }
 }
